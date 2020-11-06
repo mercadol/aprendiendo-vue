53e313402fa22482d15ed55a9d43935046cdc1aa
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-06 13:39:09 -0500

    Slider and sidebar basic

---
+++ src/components/SliderComponent.vue
@@ -0,0 +1,12 @@
+<template>
+<div id="slider" class="slider-big">
+    <h1>Sean todos Bienvenido y disfruten del contenido</h1>
+    <a href="#" class="btn-white">Ir al blog</a>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SliderComponent'
+}
+</script>
