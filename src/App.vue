bfff72cf53b7594752f1a3dde0093459d16b649e
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-06 13:33:50 -0500

    Inicio de aplicacion con Helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <HeaderComponent></HeaderComponent>
+    <Header></Header>
+    <slider-component></slider-component>
+    <div class="center">
+        <last-articles></last-articles>
+        <sidebar></sidebar>
+    </div>
+    <div class="clearfix"></div>
+    <footer-component></footer-component>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import MiComponente from '@/components/MiComponente'
+import HeaderComponent from './components/HeaderComponent.vue';
+import SliderComponent from '@/components/SliderComponent';
+import Sidebar from '@/components/Sidebar'
+import LastArticles from '@/components/LastArticles'
+import FooterComponent from '@/components/FooterComponent'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HeaderComponent,
+        SliderComponent,
+        Sidebar,
+        LastArticles,
+        FooterComponent
+    }
 }
 </script>
 
 <style>
+/*
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
+*/
+@import './assets/css/styles.css'
 </style>
