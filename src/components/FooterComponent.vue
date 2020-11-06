e2eda69bb5e432d11161fc1aeec249123645fd1c
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-06 13:39:29 -0500

    mi Footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<footer id="footer">
+    <div class="center">
+        <p>
+            &copy; Leonardo Mercado 2020
+        </p>
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
