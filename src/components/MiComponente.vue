1c1d3a46a6eb4f430d1bad710c816685cbe01e09
Leonardo Mercado <lamercadob1@gmail.com>
2020-11-06 13:39:53 -0500

    MiComponente

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="mi-componente">
+    <h1>Este es MI COMPONENTE</h1>
+    <h2>{{subtitulo}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data() {
+        return {
+            subtitulo: 'Soy el Subtitulo de mi componente'
+        }
+    }
+}
+</script>
